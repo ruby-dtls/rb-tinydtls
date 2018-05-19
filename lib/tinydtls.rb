e97e1bc89457177fed05deadd36182ef944f019f
Sören Tempel <tempel@uni-bremen.de>
2018-05-19 15:05:09 +0200

    tinydtls: require ffi.rb and udpsocket.rb

--- lib/tinydtls.rb
+++ lib/tinydtls.rb
@@ -1,3 +1,12 @@
+require "socket"
+require "ffi"
+
+require "tinydtls/wrapper"
+require "tinydtls/udpsocket"
+
 module TinyDTLS
-  # …
+  # Map used to map `object_ids` passed as void pointers to the tinydtls
+  # callback functions to actually ruby UDPSockets. This is neccessary
+  # since we can't pass pointers to ruby objects to C functions.
+  SOCKET_MAP = Hash.new
 end
