b54aed054df2192a1277380e46bfccc3808599ef
Sören Tempel <tempel@uni-bremen.de>
2018-05-25 19:23:18 +0200

    examples: configure a PSK in dtls-server.rb

--- examples/dtls-server.rb
+++ examples/dtls-server.rb
@@ -3,6 +3,9 @@ require "tinydtls"
 socket = TinyDTLS::UDPSocket.new(Socket::AF_INET6)
 socket.bind("::1", 2342)
 
+socket.add_key("foobar")
+socket.add_key("foobar", "foobar")
+
 while true
   msg = socket.recvfrom
   puts "Received: #{msg}"
