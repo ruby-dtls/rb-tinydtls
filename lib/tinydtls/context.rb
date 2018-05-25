d56e3ea9274f3bba04dc9c49ff9b1f8be501b2b4
Sören Tempel <tempel@uni-bremen.de>
2018-05-25 19:22:23 +0200

    context: add identity map for mapping identities to PSKs

--- lib/tinydtls/context.rb
+++ lib/tinydtls/context.rb
@@ -8,14 +8,32 @@ module TinyDTLS
     # The queue used for communication with the receive thread.
     attr_reader :queue
 
+    # The default key, used when the request didn't contain a hint.
+    attr_reader :default_key
+
     def initialize(socket, queue)
       @socket = socket
       @queue  = queue
+
+      @idmap = Hash.new
+      @default_key = nil
     end
 
     def self.from_ptr(ptr)
       obj = Wrapper::DTLSContextStruct.new(ptr)
       return CONTEXT_MAP[Wrapper::dtls_get_app_data(obj).to_i]
+    end
+
+    def add_key(identity, key)
+      if identity.nil?
+        @default_key = key
+      end
+
+      @idmap[identity] = key
+    end
+
+    def get_key(identity)
+      @idmap[identity]
     end
   end
 end
