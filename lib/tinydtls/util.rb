3d0c127a5e7f5dca583557162b1f4192cc43a443
Sören Tempel <tempel@uni-bremen.de>
2018-05-28 10:49:04 +0200

    util: add wrapper for dtls_write taking an instance of the Addrinfo class

--- lib/tinydtls/util.rb
+++ lib/tinydtls/util.rb
@@ -3,5 +3,17 @@ module TinyDTLS
     def self.byteslice(str, len)
       return len >= 0 ? str.byteslice(0, len) : str
     end
+
+    def self.dtls_send(ctx, mesg, addrinfo)
+      sess = Wrapper::dtls_new_session(
+        addrinfo.afamily, addrinfo.ip_port, addrinfo.ip_address)
+
+      res = Wrapper::dtls_write(ctx, sess, mesg, mesg.bytesize)
+      if res == -1
+        raise Errno::EIO
+      end
+
+      return res
+    end
   end
 end
