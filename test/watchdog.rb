a1c795ff0018a22dce4dff2909fe9fd03a0a62a7
Sören Tempel <tempel@uni-bremen.de>
2018-06-18 13:52:29 +0200

    test: ensure that watchdog marks peers as used

--- test/watchdog.rb
+++ test/watchdog.rb
@@ -16,17 +16,33 @@ class TestWatchdog < Utility
     @client_socket.close
   end
 
+  def test_peer_marked_as_used
+    @client_socket.send("foobar", 0, TEST_HOST, TEST_SERVER_PORT)
+    sessions = get_client_sessions
+
+    assert_equal 1, sessions.size
+    # FIXME: does RDNS resolution
+    # assert_equal [TEST_HOST, TEST_SERVER_PORT],
+    #   sessions.keys.first
+
+    assert sessions.values.first.last
+  end
+
   def test_free_stale_peer
     @client_socket.send("foobar", 0, TEST_HOST, TEST_SERVER_PORT)
-    assert_equal 1,
-      @client_socket.instance_variable_get("@sess_hash").size
+    assert_equal 1, get_client_sessions.size
 
     # We don't really know how long it takes until the thread is
     # actually scheduled so we just add a few seconds for good
     # measure.
     sleep 5 * TEST_TIMEOUT
 
-    assert_equal 0,
-      @client_socket.instance_variable_get("@sess_hash").size
+    assert_equal 0, get_client_sessions.size
+  end
+
+  private
+
+  def get_client_sessions
+    @client_socket.instance_variable_get("@sess_hash")
   end
 end
