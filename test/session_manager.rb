0a2995cf2d11fb6736326c266a3610c3fa769473
Sören Tempel <tempel@uni-bremen.de>
2018-07-17 13:27:46 +0200

    test: add tests for SessionManager

---
+++ test/session_manager.rb
@@ -0,0 +1,42 @@
+class TestSessionManager < Utility
+  TEST_TIMEOUT = 5.freeze
+
+  def setup
+    ctx = TinyDTLS::Wrapper::DTLSContextStruct.new(nil)
+    @sessions = TinyDTLS::SessionManager.new(ctx, TEST_TIMEOUT)
+  end
+
+  def test_add_and_check_used
+    addrinfo = Addrinfo.getaddrinfo("www.kame.net", 80).first
+    @sessions[addrinfo] do |sess|
+      assert_equal addrinfo, sess.addrinfo
+    end
+
+    sessions = get_session_store
+    assert_equal 1, sessions.size
+    assert_used sessions.values.first
+  end
+
+  def test_marked_as_unused
+    addrinfo = Addrinfo.getaddrinfo("www.google.com", 80).first
+    @sessions[addrinfo] { |s| nil }
+
+    assert_used get_session_store.values.first
+    sleep TEST_TIMEOUT
+    assert_unused get_session_store.values.first
+  end
+
+  def test_free_unused
+    addrinfo = Addrinfo.getaddrinfo("www.ruby-lang.org", 443).first
+    @sessions[addrinfo] { |s| nil }
+
+    sleep TEST_TIMEOUT * 2
+    assert get_session_store.empty?
+  end
+
+  private
+
+  def get_session_store
+    @sessions.instance_variable_get("@store")
+  end
+end
--- test/test.rb
+++ test/test.rb
@@ -3,7 +3,10 @@ require "minitest/autorun"
 require "socket"
 require "tinydtls"
 
-require_relative "./security_conf.rb"
 require_relative "./utility.rb"
+
+require_relative "./security_conf.rb"
+require_relative "./session_manager.rb"
+
 require_relative "./udpsocket.rb"
 require_relative "./watchdog.rb"
